b0c1e9e09ecbbf44d7957cd15aa6427ecebd6d43
Dio Gonzalez <dionisggr@gmail.com>
2021-02-13 18:26:10 -0700

    added landing page

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const morganLogger = require('./loggers/morgan-logger');
 const errorHandler = require('./error-handlers/error-handler');
 const { authorization } = require('./helpers/validation');
+const LandingRouter = require('./routers/landing-router');
 const UserRouter = require('./routers/user-router');
 const ListRouter = require('./routers/list-router');
 const MovieRouter = require('./routers/movie-router');
@@ -17,11 +18,11 @@ app.use(cors());
 app.use(morganLogger);
 app.use(express.json());
 
-app.use(authorization);
-app.use('/api/users', UserRouter);
-app.use('/api/lists', ListRouter);
-app.use('/api/movies', MovieRouter);
-app.use('/api', AccessRouter);
+app.use('/', LandingRouter);
+app.use('/api/users', authorization, UserRouter);
+app.use('/api/lists', authorization, ListRouter);
+app.use('/api/movies', authorization, MovieRouter);
+app.use('/api', authorization, AccessRouter);
 app.use(errorHandler);
 
 module.exports = app;
