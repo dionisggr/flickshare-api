0fe87b993eb91ebd6f4204412b035682a60c837c
Dio Gonzalez <dionisggr@gmail.com>
2021-02-23 11:53:37 -0700

    changes

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  API_KEY: process.env.API_KEY || 'my-secret-key',
-  ADMIN_KEY: process.env.ADMIN_KEY || 'my-secret-key',
+  API_KEY: process.env.API_KEY || 'my-secret-admin',
+  ADMIN_KEY: process.env.ADMIN_KEY || 'my-secret-admin',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
   JWT_SECRET: process.env.JWT_SECRET || 'my-secret-jwt',
   DATABASE_URL: process.env.DATABASE_URL || "postgresql://admin@localhost/flickshare",
